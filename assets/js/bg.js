b35b816f0e95fa187e3a00a1398affa7b43c5217
FabledIndiaGames <bugmenotramu@yandex.com>
2024-10-20 17:41:14 +0530

    Delete assets/js/bg.js

--- assets/js/bg.js
+++
@@ -1,7 +0,0 @@
-// background.js
-
-// Set the relative path to your image
-const imagePath = "images/bg.jpg"; // Adjust this path as needed
-
-// Set the background image
-document.body.style.backgroundImage = `url('${imagePath}')`;
