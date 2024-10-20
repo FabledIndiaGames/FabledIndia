d897f2f657d6f2b37d16a59941b2b2b05161a2d0
FabledIndiaGames <bugmenotramu@yandex.com>
2024-10-20 17:30:58 +0530

    Add files via upload

---
+++ assets/js/bg.js
@@ -0,0 +1,7 @@
+// background.js
+
+// Set the relative path to your image
+const imagePath = "images/bg.jpg"; // Adjust this path as needed
+
+// Set the background image
+document.body.style.backgroundImage = `url('${imagePath}')`;
